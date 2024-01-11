3a51df0010a438f9e6a9d0db8399da673907ef53
Paul T <102877038+ptd3v@users.noreply.github.com>
2024-01-11 08:38:49 +0000

    Reworded Parameters

--- write-a-function.py
+++ write-a-function.py
@@ -9,8 +9,12 @@ def is_leap(year):
         
     #Why is this returning false? Lets check the three condidions.
     #Condition 1: The year can be evenly divided by 4, is a leap year, unless:
+        #Reworded: if year%4 == 0, leap = True
     #Condition 2: The year can be evenly divided by 100, it is NOT a leap year, unless:
+        #Reworded: if year%100 == 0, leap = False
     #Condition 3: The year is also evenly divisible by 400. Then it is a leap year.
+        #Reworded: if year%4==0 and year%400 == 0, leap = True
+        
         return True
     else:
         return False
